cf33017e6074c5f169ca4fe7e29fc72e3e19ad6f
mengjuesun <ms6729a@student.american.edu>
2020-09-21 14:55:38 -0400

    PatternAssignment
    Assignment2

---
+++ Mengjue Sun_Te WangAssignment1/Assets/Scripts/Enemy.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.IO;
+using UnityEngine;
+
+public class Enemy : MonoBehaviour
+{
+
+    private string mName;
+    private Vector3 mScale;
+
+    void Start()
+    {
+        ReadRandomNameAndScale(Application.dataPath + "\\Enemy.txt");
+        
+    }
+
+
+    private void ReadRandomNameAndScale(string path)
+    {
+        StreamReader sr = new StreamReader(path);
+        string line;
+        int target = Random.Range(0, 6) * 4;
+        Debug.Log(target);
+        int i = 0;
+        while ((line = sr.ReadLine()) != null)
+        {
+            if (i == target)
+            {
+                mName = line;
+                mScale.x = float.Parse(sr.ReadLine());
+                mScale.y = float.Parse(sr.ReadLine());
+                mScale.z = float.Parse(sr.ReadLine());
+                break;
+            }
+            i++;
+        }
+        transform.localScale = mScale;
+        name = mName;
+    }
+
+    void Update()
+    {
+        transform.Translate(-Time.deltaTime * Vector3.up * 2);
+    }
+}
